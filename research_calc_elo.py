8d6d89034982781176c2a14561af2da0e7a6388e
Szymon Idec <szymon.idec@protonmail.com>
2024-02-28 00:23:22 +0100

    aaa

--- research_calc_elo.py
+++ research_calc_elo.py
@@ -42,8 +42,8 @@ def elo_update(elo_a, elo_b, result, k=32):
     return new_a, new_b
 
 
-if __name__ == '__main__':
-    with open('game_history/random-mcts32-mcts128-dds-bzero.pkl', 'rb') as f:
+if __name__ == "__main__":
+    with open("game_history/random-mcts32-mcts128-dds-bzero.pkl", "rb") as f:
         game_history = pickle.load(f)
 
     num_players = 5
@@ -61,30 +61,43 @@ if __name__ == '__main__':
     for _ in tqdm(range(nperm)):
         elo_ratings = np.ones(num_players) * 1000
         for p0, p1, result in rng.permutation(game_history):
-            elo_ratings[p0], elo_ratings[p1] = elo_update(elo_ratings[p0], elo_ratings[p1], result, 1)
+            elo_ratings[p0], elo_ratings[p1] = elo_update(
+                elo_ratings[p0], elo_ratings[p1], result, 1
+            )
         perm_elo_ratings.append(elo_ratings)
     perm_elo_ratings = np.array(perm_elo_ratings)
 
-    df = pd.DataFrame({
-        "player": player_names,
-        "mean_elo": np.mean(perm_elo_ratings, axis=0),
-        "std_elo": np.std(perm_elo_ratings, axis=0),
-    })
-    df.to_csv('research_out/calc_elo.csv', index=False)
+    df = pd.DataFrame(
+        {
+            "player": player_names,
+            "mean_elo": np.mean(perm_elo_ratings, axis=0),
+            "std_elo": np.std(perm_elo_ratings, axis=0),
+        }
+    )
+    df.to_csv("research_out/calc_elo.csv", index=False)
 
-    plt.figure()
-    plt.boxplot(perm_elo_ratings)
-    plt.xticks(range(1, num_players + 1), player_names, rotation=45)
-    plt.savefig('research_out/calc_elo_boxplot.png')
-
-    plt.figure()
     colors = {
-        'random': 'C0',
-        'mcts-32': 'C1',
-        'mcts-128': 'C2',
-        'dds': 'C3',
-        'bzero': 'C4',
+        "random": "C0",
+        "mcts-32": "C1",
+        "mcts-128": "C2",
+        "dds": "C3",
+        "bzero": "C4",
     }
+
+    fig, ax = plt.subplots()
+    bplot = ax.boxplot(
+        perm_elo_ratings,
+        patch_artist=True,
+        labels=player_names,
+    )
+    for patch, color in zip(bplot["boxes"], [colors[p] for p in player_names]):
+        patch.set_facecolor(color)
+    ax.yaxis.grid(True)
+
+    plt.savefig("research_out/calc_elo_boxplot.png")
+
+    plt.figure()
+
     rng = np.random.default_rng(0)
     for _ in range(3):
         elo_rating = [np.ones(num_players) * 1000]
@@ -96,4 +109,4 @@ if __name__ == '__main__':
         for i in range(num_players):
             plt.plot(elo_rating[:, i], color=colors[player_names[i]], alpha=0.5)
     plt.legend(player_names)
-    plt.savefig('research_out/calc_elo_history.png')
+    plt.savefig("research_out/calc_elo_history.png")
